e397e7078372132da0725486e098c6da57bbb94b
yt2226creer <tuji6222home@gmail.com>
2025-07-09 22:37:14 +0900

    sss

--- Chapter.6/script.js
+++ Chapter.6/script.js
@@ -1,7 +1,7 @@
-let first = "TOKYO";
-if(first === TOKYO){
-    console.log(true);
-}else if(first === OSAKA){
+const first = "TOKYO";
+if(first === "TOKYO"){
+    console.log(first);
+}else if(first === "OSAKA"){
     console.log("第二都市");
 }else{
     console.log("不正解");
