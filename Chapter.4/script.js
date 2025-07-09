449ea2ed91834cb04ea96ffc4cd50b984ca2bd72
yt2226creer <tuji6222home@gmail.com>
2025-07-09 21:34:51 +0900

    2

--- Chapter.4/script.js
+++ Chapter.4/script.js
@@ -1,24 +1,4 @@
-let first = 1 ;
-console.log(first);
-
-let second = "second"  ;
-console.log(second) ;
-
-let third = true ;
-console.log(third) ;
-
-let forth = 10+20  ;
-console.log(forth) ;
-
-let fifth = 20-10  ;
-console.log(first) ;
-
-let sixth = 50*2 ;
-console.log(sixth) ;
-
-let seventh = 20/2 ;
-console.log(seventh) ;
-
-let eighth = "12"+"34" ;
-console.log(eighth ) ;//
-
+const metalbirst = (lastDamage) => {
+    return = lastDamage * 1.5 ;
+    console.log(50);
+}
