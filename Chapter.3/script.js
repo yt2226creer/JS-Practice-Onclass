6f4921a5d1f3f3033ceadba7940776df1d11c342
yt2226creer <tuji6222home@gmail.com>
2025-07-09 21:22:38 +0900

    課題

---
+++ Chapter.3/script.js
@@ -0,0 +1,24 @@
+let first = 1 ;
+console.log(first) ;
+
+let second = "second"  ;
+console.log(second) ;
+
+let third = true ;
+console.log(third) ;
+
+let forth = 10+20  ;
+console.log(forth) ;
+
+let fifth = 20-10  ;
+console.log(first) ;
+
+let sixth = 50*2 ;
+console.log(sixth) ;
+
+let seventh = 20/2 ;
+console.log(seventh) ;
+
+let eighth = "12"+"34" ;
+console.log(eighth ) ;
+
