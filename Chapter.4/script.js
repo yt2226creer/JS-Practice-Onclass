0a227add47448a02a1aff2ea89b6784c8cbe674b
yt2226creer <tuji6222home@gmail.com>
2025-07-09 22:01:30 +0900

    aaa

--- Chapter.4/script.js
+++ Chapter.4/script.js
@@ -1,4 +1,16 @@
 let bar = true;
 if(bar === true){
-    console.log(true);
+    console.log(bar);
 }
+
+let bar = 100;
+if(bar === true){
+    console.log(bar);
+}
+
+
+let bar = "JavaScript";
+if(bar === true){
+    console.log(bar);
+}
+
