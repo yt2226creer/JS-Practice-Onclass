ff17460657f7fbb53add0b761d0fa261f09d58d1
yt2226creer <tuji6222home@gmail.com>
2025-07-09 21:44:14 +0900

    dddddd

--- Chapter.4/script.js
+++ Chapter.4/script.js
@@ -1,5 +1,6 @@
 const metalbirst = (lastDamage) => {
-    metalbirst = lastDamage * 1.5 
-    if(lastDamage === 50) 
+    metalbirst = lastDamage * 1.5 ;
+    if(lastDamage === 50) {
     console.log(metalbirst) ;
+    }
 }
