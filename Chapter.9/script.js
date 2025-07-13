ed3d63245ebde01b862b8318a2b4812710ebd73d
yt2226creer <tuji6222home@gmail.com>
2025-07-13 23:12:38 +0900

    2

--- Chapter.9/script.js
+++ Chapter.9/script.js
@@ -1,8 +1,9 @@
 var boolean = true;
-var resultText = "";
-if (boolean) {
- resultText = "Aの処理"
-} else {
- resultText = "Bの処理"
-};
+//var resultText = "";
+//if (boolean) {
+ //resultText = "Aの処理"
+//} else {
+ //resultText = "Bの処理"
+//};
+const resultText = boolean ? "A" : "B";
 console.log(resultText);
