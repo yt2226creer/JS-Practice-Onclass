514c142c16129f9b8def33604d8c98a8651fbbc0
yt2226creer <tuji6222home@gmail.com>
2025-07-13 22:56:22 +0900

    7/13

---
+++ Chapter.8/script.js
@@ -0,0 +1,22 @@
+const x = 10;
+const y = 25;
+
+//1
+if(x > y){
+    console.log(true);
+}
+
+//2
+if(x < y){
+    console.log(true);
+}
+
+//3
+if(x >= y){
+    console.log(true);
+}
+
+//4
+if(x <= y){
+    console.log(true);
+}
