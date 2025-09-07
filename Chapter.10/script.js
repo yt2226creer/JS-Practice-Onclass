6f14074d9c795bcd83a862099e9b9a14daade341
yt2226creer <tuji6222home@gmail.com>
2025-09-07 23:06:59 +0900

    333

--- Chapter.10/script.js
+++ Chapter.10/script.js
@@ -1,2 +1,25 @@
 const fruits = [ "リンゴ" ,"バナナ" ,"イチゴ"];
-console.log(fruits);
+console.log(fruits[2]);
+
+/////
+
+///keyはオブジェクトにおいて呼び出すための重要な要素であり、Valueはkeyの要素の値となる。それらをまとめてプロパティーと呼ぶ。
+
+//3.
+
+const CarMaker =[
+    {name: "TOYOTA"
+     from: "Japan"
+     share:"No.1"
+    },
+    {name: "Ford"
+    from: "America"
+    share:"No.10"
+    },
+    {name: "Mercedeace"
+     from:"Germany"
+     share:"NO.2"
+    },
+];
+
+console.log{Carmeaker.Ford};
