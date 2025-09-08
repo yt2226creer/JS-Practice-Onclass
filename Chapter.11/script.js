15faae2e62452092fa0d77b20352355d6934b230
yt2226creer <tuji6222home@gmail.com>
2025-09-08 12:38:52 +0900

    aa

--- Chapter.11/script.js
+++ Chapter.11/script.js
@@ -1,4 +1,22 @@
-var items = ["リンゴ", "バナナ", "イチゴ"];
+const items = ["リンゴ", "バナナ", "イチゴ"];
 for (index in items) {
  console.log(items[index]);
 };
+
+const teachers = [ 
+    { name: "太郎", 
+        age: 25, 
+        subject: "国語", 
+    },
+     { name: "浩司", 
+        age:30, 
+        subject: "数学", 
+    },
+         { name: "花⼦", 
+            age: 26, subject: 
+            "社会", 
+        } 
+        ] 
+        for (teacher of teachers)  { 
+            console.log(teacher);
+        }; 
