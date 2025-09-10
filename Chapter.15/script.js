7b85a5e2b97b15f97e54ffc54068fe8feadd6bcf
yt2226creer <tuji6222home@gmail.com>
2025-09-10 19:28:28 +0900

    ９/１０.最終

--- Chapter.15/script.js
+++ Chapter.15/script.js
@@ -3,7 +3,24 @@
 //例
 //2.
 function fruit(name, price) {
- const result = name + "の値段は" + price + "円です。"
- return result;
+ const result = name + "の値段は" +  price + "円です。"
+ return result ;
 };
-fruit("リンゴ", "210");
+console.log(fruit("リンゴ", "210"));
+
+
+
+function addTax(price, func) {
+  const taxPrice = Math.round(price * 1.10);
+  func(taxPrice);
+}
+
+//トマトの値段
+addTax(200, function(taxPrice) {
+  console.log("トマトの値段は税込み" + taxPrice + "円");
+});
+
+// ②玉ねぎの値段
+addTax(150, function(taxPrice) {
+  console.log("玉ねぎの値段は税込み" + taxPrice + "円");
+});
