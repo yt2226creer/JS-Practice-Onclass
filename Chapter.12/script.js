cdcc2da63512f9e9819a983d97277fd5a7895ecd
yt2226creer <tuji6222home@gmail.com>
2025-09-10 18:27:55 +0900

    9/10.最終

--- Chapter.12/script.js
+++ Chapter.12/script.js
@@ -1,4 +1,20 @@
-const getText = function () {
- return "getTextが実⾏されたのでこのテキストを返します。"
+//1.
+//const  printAlert = () => {
+    //alert("印刷ができません")
+//};
+//printAlert();
+
+//2.
+const iphoneValue = (firstPrice) =>{
+    if(firstPrice === 129800){
+     return firstPrice * 0.9;
+     }
+    return firstPrice ;
 };
-console.log(getText());
+
+console.log(iphoneValue(129800));
+console.log(iphoneValue(100000));
+
+//3.
+スコープとは様々な関数を一つの案件ごとに設けるとき、なんでもグローバルな変数で宣言してしまうと、
+意図しないところで変数名が干渉してしまったりするので、そのような事態を防ぐための決まりである。
