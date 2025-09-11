37bb62516c8e3bbff898b7e0cad354c56e0b5315
Yuzuru Tsujigaito <tuji6222home@gmail.com>
2025-09-11 21:52:01 +0900

    ９．１１　最終

--- Chapter.16/script.js
+++ Chapter.16/script.js
@@ -1,2 +1,17 @@
-const mainTitle = document.getElementById("main-title")
-console.log(mainTitle);
+const wrapper = document.getElementById("wrapper")
+
+const newBox = `<div class="new-box">new-box要素</div>`
+
+setTimeout(function () {
+ // wrapper要素内先頭にnewBoxに代⼊されたHTML要素を挿⼊
+
+ wrapper.insertAdjacentHTML("afterbegin", newBox)
+
+}, 3000);
+
+setTimeout(function () {
+ // wrapper要素内先頭にnewBoxに代⼊されたHTML要素を挿⼊
+ wrapper.insertAdjacentHTML("afterbegin", newBox)
+ // wrapper要素の直後にnewBoxに代⼊されたHTML要素を挿⼊
+ wrapper.insertAdjacentHTML("afterend", newBox)
+}, 3000);
